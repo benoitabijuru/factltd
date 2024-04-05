f7c4474b593bc5de7ce6d2fe65bbb64eabf51536
Philimuhire <philimuhire@gmail.com>
2024-04-05 12:12:25 +0200

    started sales page

--- FactWebApp/urls.py
+++ FactWebApp/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for FactWeb project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from FactWebApp import views
 
@@ -28,5 +13,7 @@ urlpatterns = [
     path('video/', views.video, name='video'),
     path('careers/', views.careers, name='careers'),
     path('research/', views.research, name='research'),
+    path('sales/', views.sales, name='sales'),
+    path('checkout/', views.checkout, name='checkout'),
     
 ]
